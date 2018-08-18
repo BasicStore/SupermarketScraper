55b65adca8d4feb9fcee944f90a8de288c81dede
Paul <paul.home47@gmail.com>
2018-08-18 16:47:43 +0100

    first commit

---
+++ src/main/java/com/sainsburys/client/ApplicationMain.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.sainsburys.client;
+
+/**
+ * @author Paul
+ *
+ */
+public class ApplicationMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		
+		
+
+	}
+
+}
